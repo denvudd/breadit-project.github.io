e2eaeef5acd7783023038c360f731dddaef95b5e
Dima <dmytro.jurin@kitu.nau.edu.ua>
2023-07-23 12:58:24 +0300

    intercepting routes for sign-in added

---
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -0,0 +1,14 @@
+import SignIn from "@/components/SignIn";
+import React from "react";
+
+interface pageProps {}
+
+const page: React.FC<pageProps> = ({}) => {
+  return (
+    <div className="fixed inset-0 bg-zinc-900/20 z-10">
+      INTERSECT
+    </div>
+  );
+};
+
+export default page;
