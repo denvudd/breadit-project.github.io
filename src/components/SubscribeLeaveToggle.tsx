d2ea9524ab071d396373f557cf1af6228deb006d
Dima <dimontv7@gmail.com>
2023-07-26 14:01:40 +0300

    subscribe to subreddit function added

--- src/components/SubscribeLeaveToggle.tsx
+++ src/components/SubscribeLeaveToggle.tsx
@@ -4,30 +4,67 @@ import React from "react";
 import { Button } from "./ui/Button";
 import { useMutation } from "@tanstack/react-query";
 import type { SubscribeToSubredditPayload } from "@/lib/validators/subreddit";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { useLoginToast } from "@/hooks/use-login-toast";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 interface SubscribeLeaveToggleProps {
   subredditId: string;
+  isSubscribed: boolean;
+  subredditName: string;
 }
 
 const SubscribeLeaveToggle: React.FC<SubscribeLeaveToggleProps> = ({
   subredditId,
+  isSubscribed,
+  subredditName,
 }) => {
-  const {} = useMutation({
+  const { loginToast } = useLoginToast();
+  const router = useRouter();
+
+  const { mutate: subscribe, isLoading: isSubscriptionLoading } = useMutation({
     mutationFn: async () => {
       const payload: SubscribeToSubredditPayload = {
         subredditId,
       };
 
-      const {data} = await axios.post('/api/subreddit/subscribe', payload);
+      const { data } = await axios.post("/api/subreddit/subscribe", payload);
       return data as string;
     },
+    onError: (error) => {
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        return loginToast();
+      }
+
+      return toast({
+        title: "There was a problem",
+        description: "Something went wrong, please try again.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      React.startTransition(() => {
+        router.refresh();
+      });
+
+      return toast({
+        title: "Subscribed",
+        description: `You are now subscribed to r/${subredditName}`,
+      });
+    },
   });
-  const isSubscribed = false;
+
   return isSubscribed ? (
     <Button className="w-full mt-1 mb-4">Leave community</Button>
   ) : (
-    <Button className="w-full mt-1 mb-4">Join to post</Button>
+    <Button
+      isLoading={isSubscriptionLoading}
+      onClick={() => subscribe()}
+      className="w-full mt-1 mb-4"
+    >
+      Join to post
+    </Button>
   );
 };
 
