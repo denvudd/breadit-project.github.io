bc0eac513e4e02f8a2f3cf6c4b6f15265ae27d63
Dima <dimontv7@gmail.com>
2023-07-26 20:03:46 +0300

    create onSubmit for form for creating post

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -8,6 +8,9 @@ import { PostValidator, type PostCreationRequest } from "@/lib/validators/post";
 import type EditorJS from "@editorjs/editorjs";
 import { uploadFiles } from "@/lib/uploadthing";
 import { toast } from "@/hooks/use-toast";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { usePathname, useRouter } from "next/navigation";
 
 interface EditorProps {
   subredditId: string;
@@ -26,7 +29,40 @@ const Editor: React.FC<EditorProps> = ({ subredditId }) => {
       content: null,
     },
   });
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      subredditId,
+    }: PostCreationRequest) => {
+      const payload: PostCreationRequest = {
+        subredditId,
+        title,
+        content,
+      };
+      const { data } = await axios.post("/api/subreddit/post/create", payload);
+      return data as string;
+    },
+    onError: () => {
+      return toast({
+        title: "Something went wrong",
+        description: "Your post was not published, please try again later.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      const newPathname = pathname.split("/").slice(0, -1).join("/");
+      router.push(newPathname);
+      router.refresh();
 
+      return toast({
+        description: "Your post has been published.",
+      });
+    },
+  });
+
+  const router = useRouter();
+  const pathname = usePathname();
   const ref = React.useRef<EditorJS>();
   const _titleRef = React.useRef<HTMLTextAreaElement>(null);
   const { ref: titleRef, ...rest } = register("title");
@@ -72,6 +108,17 @@ const Editor: React.FC<EditorProps> = ({ subredditId }) => {
       });
     }
   }, [errors]);
+
+  const onSubmit = async (data: PostCreationRequest) => {
+    const blocks = await ref.current?.save();
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    };
+
+    createPost(payload);
+  };
 
   const initEditor = React.useCallback(async () => {
     const EditorJS = (await import("@editorjs/editorjs")).default;
@@ -135,7 +182,7 @@ const Editor: React.FC<EditorProps> = ({ subredditId }) => {
       <form
         id="subreddit-post-form"
         className="w-fit"
-        onSubmit={handleSubmit((e) => {})}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
