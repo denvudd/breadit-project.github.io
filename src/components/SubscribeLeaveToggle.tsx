eebefe3e3e52d268e24daa0c6eb4667e0527d9ac
Dima <dimontv7@gmail.com>
2023-07-26 14:13:11 +0300

    unsibscribe api route added, leave from subreddit function added

--- src/components/SubscribeLeaveToggle.tsx
+++ src/components/SubscribeLeaveToggle.tsx
@@ -55,8 +55,50 @@ const SubscribeLeaveToggle: React.FC<SubscribeLeaveToggleProps> = ({
     },
   });
 
+  const { mutate: unsubscribe, isLoading: isUnsubscriptionLoading } =
+    useMutation({
+      mutationFn: async () => {
+        const payload: SubscribeToSubredditPayload = {
+          subredditId,
+        };
+
+        const { data } = await axios.post(
+          "/api/subreddit/unsubscribe",
+          payload
+        );
+        return data as string;
+      },
+      onError: (error) => {
+        if (error instanceof AxiosError && error.response?.status === 401) {
+          return loginToast();
+        }
+
+        return toast({
+          title: "There was a problem",
+          description: "Something went wrong, please try again.",
+          variant: "destructive",
+        });
+      },
+      onSuccess: () => {
+        React.startTransition(() => {
+          router.refresh();
+        });
+
+        return toast({
+          title: "Unsubscribed",
+          description: `You are now unsubscribed from r/${subredditName}`,
+        });
+      },
+    });
+
   return isSubscribed ? (
-    <Button className="w-full mt-1 mb-4">Leave community</Button>
+    <Button
+      isLoading={isUnsubscriptionLoading}
+      onClick={() => unsubscribe()}
+      className="w-full mt-1 mb-4"
+    >
+      Leave community
+    </Button>
   ) : (
     <Button
       isLoading={isSubscriptionLoading}
