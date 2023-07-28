70c6d32faa85a2bf23add2545fbf580142c50330
Dima <dimontv7@gmail.com>
2023-07-28 21:49:34 +0300

    create subreddit/post/vote api route

---
+++ src/app/api/subreddit/post/vote/route.ts
@@ -0,0 +1,70 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { PostVoteValidator } from "@/lib/validators/vote";
+
+const CACHE_AFTER_UPVOTES = 1;
+
+export async function PATCH(req: Request) {
+  try {
+    const body = req.json();
+
+    const { postId, voteType } = PostVoteValidator.parse(body);
+
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const existingVote = await db.vote.findFirst({
+      where: {
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+    const post = await db.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+        votes: true,
+      },
+    });
+
+    if (!post) {
+      return new Response("Post not found", { status: 404 });
+    }
+
+    if (existingVote) {
+      // each user can vote 1 time per 1 post and if he votes again with the same type then a vote will delete
+      if (existingVote.type === voteType) {
+        await db.vote.delete({
+          where: {
+            userId_postId: {
+              postId,
+              userId: session.user.id,
+            },
+          },
+        });
+
+        return new Response("Your vote has been registered successfully", {
+          status: 200,
+        });
+      }
+
+      await db.vote.update({
+        where: {
+          userId_postId: {
+            postId,
+            userId: session.user.id,
+          },
+        },
+        data: {
+          type: voteType,
+        },
+      });
+    }
+  } catch (error) {}
+}
