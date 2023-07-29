f3f15c0967436936e4a69740e391d0eb54e1f36c
Dima <dimontv7@gmail.com>
2023-07-29 17:17:43 +0300

    create suspense for post details vote, PostVoteServer componentn implemented

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,5 +1,9 @@
+import EditorOutput from "@/components/EditorOutput";
+import PostVoteServer from "@/components/posts-vote/PostVoteServer";
+import PostVoteShell from "@/components/posts-vote/PostVoteShell";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
+import { formatTimeToNow } from "@/lib/utils";
 import type { CachedPost } from "@/types/redis";
 import type { Post, User, Vote } from "@prisma/client";
 import { notFound } from "next/navigation";
@@ -37,9 +41,39 @@ const page = async ({ params }: PageProps) => {
 
   if (!post && !cachedPost) return notFound();
 
-  return <div>
-    <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between"></div>
-  </div>;
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <React.Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error server component */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </React.Suspense>
+
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 founded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
+          <EditorOutput content={post?.content ?? cachedPost.content} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/posts-vote/PostVoteShell.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { buttonVariants } from "../ui/Button";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+
+const PostVoteShell = () => {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+};
+
+export default PostVoteShell;
