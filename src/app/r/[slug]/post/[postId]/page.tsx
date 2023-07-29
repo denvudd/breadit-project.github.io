88c7a489fd3c124e1ab232845aef888004cee205
Dima <dimontv7@gmail.com>
2023-07-29 17:54:01 +0300

    add subreddit name for post details, change the structure of post details

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -5,7 +5,7 @@ import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
 import { formatTimeToNow } from "@/lib/utils";
 import type { CachedPost } from "@/types/redis";
-import type { Post, User, Vote } from "@prisma/client";
+import type { Post, Subreddit, User, Vote } from "@prisma/client";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -25,7 +25,7 @@ const page = async ({ params }: PageProps) => {
     `post: ${params.postId}`
   )) as CachedPost;
 
-  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+  let post: (Post & { votes: Vote[]; author: User, subreddit: Subreddit }) | null = null;
 
   if (!cachedPost) {
     post = await db.post.findFirst({
@@ -35,6 +35,7 @@ const page = async ({ params }: PageProps) => {
       include: {
         votes: true,
         author: true,
+        subreddit: true,
       },
     });
   }
@@ -43,33 +44,51 @@ const page = async ({ params }: PageProps) => {
 
   return (
     <div>
-      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
-        <React.Suspense fallback={<PostVoteShell />}>
-          {/* @ts-expect-error server component */}
-          <PostVoteServer
-            postId={post?.id ?? cachedPost.id}
-            getData={async () => {
-              return await db.post.findUnique({
-                where: {
-                  id: params.postId,
-                },
-                include: {
-                  votes: true,
-                },
-              });
-            }}
-          />
-        </React.Suspense>
+      <div className="rounded-md bg-white shadow">
+        <div className="p-4 flex justify-between">
+          <React.Suspense fallback={<PostVoteShell />}>
+            {/* @ts-expect-error server component */}
+            <PostVoteServer
+              postId={post?.id ?? cachedPost.id}
+              getData={async () => {
+                return await db.post.findUnique({
+                  where: {
+                    id: params.postId,
+                  },
+                  include: {
+                    votes: true,
+                  },
+                });
+              }}
+            />
+          </React.Suspense>
+          <div className="w-0 flex-1">
+            <div className="max-h-40 mt-1 text-xs text-gray-500">
+              {post?.subreddit.name ? (
+                <>
+                  <a
+                    href={`/r/${post.subreddit.name}`}
+                    className="underline text-zinc-900 text-sm underline-offset-2"
+                  >
+                    r/{post.subreddit.name}
+                  </a>
+                  <span className="px-1">•</span>
+                </>
+              ) : null}
+              <span>
+                Posted by u/{post?.author.username ?? cachedPost.authorUsername}
+              </span>{" "}
+              {formatTimeToNow(
+                new Date(post?.createdAt ?? cachedPost.createdAt)
+              )}
+            </div>
 
-        <div className="sm:w-0 w-full flex-1 bg-white p-4 founded-sm">
-          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
-            Posted by u/{post?.author.username ?? cachedPost.authorUsername}
-            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
-          </p>
-          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
-            {post?.title ?? cachedPost.title}
-          </h1>
-          <EditorOutput content={post?.content ?? cachedPost.content} />
+            <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+              {post?.title ?? cachedPost.title}
+            </h1>
+
+            <EditorOutput content={post?.content ?? cachedPost.content} />
+          </div>
         </div>
       </div>
     </div>
