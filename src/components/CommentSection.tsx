77539c3317cc6a7632befeca4ec365d635ff7bca
Dima <dimontv7@gmail.com>
2023-08-01 12:15:11 +0300

    create CommentVotes component

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -54,7 +54,12 @@ const CommentSection = async ({ postId }: CommentSectionProps) => {
             return (
               <div key={topLevelComment.id} className="flex flex-col">
                 <div className="mb-2">
-                  <PostComment comment={topLevelComment} />
+                  <PostComment
+                    postId={postId}
+                    currentVote={topLevelCommentVote}
+                    votesAmount={topLevelCommentVotesAmount}
+                    comment={topLevelComment}
+                  />
                 </div>
               </div>
             );
