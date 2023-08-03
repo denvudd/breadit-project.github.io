cd00c320c207671a14ed2c33483b20a04f2bdab0
Dima <dimontv7@gmail.com>
2023-08-03 14:00:30 +0300

    username form error handling, finished jsx

--- src/components/UserNameForm.tsx
+++ src/components/UserNameForm.tsx
@@ -5,6 +5,17 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "@prisma/client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/Card";
+import { Label } from "./ui/Label";
+import { Input } from "./Input";
+import { Button } from "./ui/Button";
 
 interface UserNameFormProps {
   user: Pick<User, "id" | "username">;
@@ -23,8 +34,37 @@ const UserNameForm: React.FC<UserNameFormProps> = ({ user }) => {
   });
 
   return (
-    <form action="">
-      UserNameForm
+    <form action="" onSubmit={handleSubmit(() => {})}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Your username</CardTitle>
+          <CardDescription>
+            Please enter a display name you are comfortable with.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="relative grid gap-1">
+            <div className="absolute top-0 left-0 w-8 h-10 grid place-items-center">
+              <span className="text-sm text-zinc-400">u/</span>
+            </div>
+            <Label className="sr-only" htmlFor="name">
+              Name
+            </Label>
+            <Input
+              id="name"
+              className="w-[400px] pl-6"
+              size={32}
+              {...register("name")}
+            />
+            {errors?.name && (
+              <p className="px-1 text-xs text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Change name</Button>
+        </CardFooter>
+      </Card>
     </form>
   );
 };
