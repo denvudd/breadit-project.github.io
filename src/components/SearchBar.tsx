fa29dfa3c4633a4603113b333eb3916dfba84701
Dima <dimontv7@gmail.com>
2023-08-03 13:23:55 +0300

    searching functionality with debounce implemented

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import React from "react";
-import { Command, CommandInput } from "./ui/Command";
+import React, { useCallback } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/Command";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import type { Prisma, Subreddit } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { Users } from "lucide-react";
+import { request } from "http";
+import debounce from "lodash.debounce";
 
 interface SearchBarProps {}
 
 const SearchBar: React.FC<SearchBarProps> = ({}) => {
   const [input, setInput] = React.useState<string>("");
+  const router = useRouter();
 
   const {
     data: queryResults,
@@ -20,13 +32,20 @@ const SearchBar: React.FC<SearchBarProps> = ({}) => {
     queryFn: async () => {
       if (!input) return [];
       const { data } = await axios.get(`/api/search?q=${input}`);
-      return data as Subreddit & {
+
+      return data as (Subreddit & {
         _count: Prisma.SubredditCountOutputType;
-      };
+      })[];
     },
     queryKey: ["search-query"],
     enabled: false,
   });
+
+  const request = debounce(() => refetch(), 150);
+
+  const debounceRequest = useCallback(() => {
+    request();
+  }, []);
 
   return (
     <Command className="relative rounded-lg border max-w-7xl z-50 overflow-visible mx-2">
@@ -36,8 +55,31 @@ const SearchBar: React.FC<SearchBarProps> = ({}) => {
         value={input}
         onValueChange={(text) => {
           setInput(text);
+          debounceRequest();
         }}
       />
+      {input.length > 0 && (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 && (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  key={subreddit.id}
+                  value={subreddit.name}
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                >
+                  <Users className="mr-2 h-2 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
+        </CommandList>
+      )}
     </Command>
   );
 };
