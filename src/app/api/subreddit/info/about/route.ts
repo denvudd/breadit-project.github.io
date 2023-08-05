bf46710e7ea945f4c9b572ad31c353569b869d48
Dima <dimontv7@gmail.com>
2023-08-05 17:22:19 +0300

    add about field for subreddit in db, create subreddit/info/about API route and validator

---
+++ src/app/api/subreddit/info/about/route.ts
@@ -0,0 +1,50 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { SubredditAboutValidator } from "@/lib/validators/subreddit";
+import { z } from "zod";
+
+export async function PATCH(req: Request) {
+  try {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const { about, subredditId } = SubredditAboutValidator.parse(body);
+
+    const subscribtionExists = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session.user.id,
+      },
+    });
+
+    if (subscribtionExists) {
+      return new Response("Subreddit doesn't exist", {
+        status: 400,
+      });
+    }
+
+    await db.subreddit.update({
+      where: {
+        id: subredditId,
+      },
+      data: {
+        about,
+      },
+    });
+
+    return new Response("About text for this subreddit has been updated successfully");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+
+    return new Response("Could not update about text, please try again later", {
+      status: 500,
+    });
+  }
+}
--- src/lib/validators/subreddit.ts
+++ src/lib/validators/subreddit.ts
@@ -12,8 +12,14 @@ export const SubredditSubscriptionValidator = z.object({
   subredditId: z.string(),
 });
 
+export const SubredditAboutValidator = z.object({
+  subredditId: z.string(),
+  about: z.string().min(1).max(500),
+});
+
 export type CreateSubredditPayload = z.infer<typeof SubredditValidator>;
 export type DeleteSubredditPayload = z.infer<typeof DeleteSubredditValidator>;
 export type SubscribeToSubredditPayload = z.infer<
   typeof SubredditSubscriptionValidator
 >;
+export type SubredditAboutPayload = z.infer<typeof SubredditAboutValidator>;
