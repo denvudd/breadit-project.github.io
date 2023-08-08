13272a7cc77eb6ed988defec4b22bce8124770f5
Dima <dimontv7@gmail.com>
2023-08-08 13:23:46 +0300

    add rules empty condition for subreddit

--- src/components/asides/SubredditRules.tsx
+++ src/components/asides/SubredditRules.tsx
@@ -24,11 +24,11 @@ const SubredditRules: React.FC<SubredditRulesProps> = ({
 }) => {
   return (
     <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-300 dark:border-gray-600 order-first md:order-last">
-      <p className="bg-white dark:bg-slate-900 px-6 py-4">
+      <p className="bg-white dark:bg-slate-900 px-6 py-4 border-b border-gray-300 dark:border-gray-600">
         r/{subredditName} rules
       </p>
       {isAuthor && (
-        <div className="flex flex-col w-full px-6 bg-white dark:bg-slate-900">
+        <div className="flex flex-col w-full px-6 py-2 bg-white dark:bg-slate-900">
           <p className="text-gray-500 dark:text-gray-300 text-sm w-full mb-2">
             Help subscribers understand the rules of this subreddit and explain
             to them how they should behave and what is not acceptable.
@@ -36,26 +36,33 @@ const SubredditRules: React.FC<SubredditRulesProps> = ({
           <AddRule subredditId={subredditId} />
         </div>
       )}
-      <Accordion
-        type="multiple"
-        className="w-full px-6 pt-4 bg-white dark:bg-slate-900 text-sm leading-6"
-      >
-        {rules.map((rule, index) => (
-          <AccordionItem key={rule.id} value={rule.id}>
-            <div className="group w-full">
-              <div className="w-full flex justify-between items-center mr-2">
-                <AccordionTrigger className="w-full justify-between gap-2">
-                  {index + 1}. {rule.name}{" "}
-                </AccordionTrigger>
-                <DeleteRule ruleId={rule.id} subredditId={subredditId} />
+      {!rules.length && (
+        <div className="divide-y divide-gray-100 dark:divide-gray-600 w-full px-6 py-2 bg-white dark:bg-slate-900 text-sm leading-6">
+          <p>Rules have not been created for this subreddit yet.</p>
+        </div>
+      )}
+      {!!rules.length && (
+        <Accordion
+          type="multiple"
+          className="w-full px-6 pt-2 bg-white dark:bg-slate-900 text-sm leading-6"
+        >
+          {rules.map((rule, index) => (
+            <AccordionItem key={rule.id} value={rule.id}>
+              <div className="group w-full">
+                <div className="w-full flex justify-between items-center mr-2">
+                  <AccordionTrigger className="w-full justify-between gap-2">
+                    {index + 1}. {rule.name}{" "}
+                  </AccordionTrigger>
+                  <DeleteRule ruleId={rule.id} subredditId={subredditId} />
+                </div>
               </div>
-            </div>
-            {rule.description && (
-              <AccordionContent>{rule.description}</AccordionContent>
-            )}
-          </AccordionItem>
-        ))}
-      </Accordion>
+              {rule.description && (
+                <AccordionContent>{rule.description}</AccordionContent>
+              )}
+            </AccordionItem>
+          ))}
+        </Accordion>
+      )}
     </div>
   );
 };
